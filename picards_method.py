5d4c5ca1ecf5222a965549fd1917b71f4babc244
devansh <devansh151005@gmail.com>
2025-03-24 23:34:30 +0530

    yes

---
+++ picards_method.py
@@ -0,0 +1,28 @@
+from sympy import *
+# print(integrate(exp(x)**2, x))
+#
+
+accuracy = 1e-6
+
+def picards_method(f, x0, y0, n, finx):
+    x, y = symbols('x y')
+    y_values = [y0]
+    it = 0
+    for i in range(n):
+        y_next = y0 + integrate(f.subs(y, y_values[-1]), (x, x0, finx))
+        y_values.append(y_next)
+        it+=1
+        if abs(y_next - y_values[-2]) < accuracy:
+            break
+    return it, y_values[-1]
+
+x, y = symbols('x y')
+
+# Given ODE: y' = x - y
+f = x**2 - y
+
+x0 = 0
+y0 = 678
+finx = 0.7
+
+print(picards_method(f, x0, y0, 1000, finx))
